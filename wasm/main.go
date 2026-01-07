12d8a67e2ab481f018e57bb24ab9a7c8d4db5410
Bjorn Gott <bbgott@gmail.com>
2026-01-07 22:38:19 +0000

    feat: implement I8080 and Z80 CPU structures with memory and register management

--- wasm/main.go
+++ wasm/main.go
@@ -3,7 +3,38 @@ package main
 import (
 	"encoding/json"
 	"syscall/js"
+
+	"github.com/bbgott/retrojs/wasm/cpu"
 )
+
+// Debugging flag
+var debugEnabled bool
+
+// Helper for memory write with debug message passing
+func memoryWrite(addr int, value byte) {
+	if emulator == nil || emulator.CPU == nil {
+		return
+	}
+	mem := emulator.CPU.GetMemory()
+	if addr < 0 || addr >= len(mem) {
+		return
+	}
+	mem[addr] = value
+	if debugEnabled {
+		// Send memoryWrite message to JS
+		msg := struct {
+			Addr  int  `json:"addr"`
+			Value byte `json:"value"`
+		}{Addr: addr, Value: value}
+		payload, _ := json.Marshal(msg)
+		sendMessageToJS("memoryWrite", string(payload))
+	}
+}
+
+// Helper to enable/disable debugging
+func setDebugEnabled(enabled bool) {
+	debugEnabled = enabled
+}
 
 // sendMessageToJS sends a message to JS with a type and payload (as string)
 func sendMessageToJS(msgType, payload string) {
@@ -22,7 +53,14 @@ type EmulatorConfig struct {
 // Emulator is a stub for the emulator core
 type Emulator struct {
 	Config EmulatorConfig
-	// TODO: Add registers, memory, peripherals, etc.
+	CPU    CPU
+}
+
+// CPU is an interface for CPU implementations
+type CPU interface {
+	GetRegisters() interface{}
+	GetMemory() []byte
+	SetRegisters(regs interface{})
 }
 
 var emulator *Emulator
@@ -35,6 +73,52 @@ func receiveMessageFromJS(_ js.Value, args []js.Value) interface{} {
 	msgType := args[0].String()
 	payload := args[1].String()
 	switch msgType {
+	case "setDebug":
+		// Enable or disable debugging
+		if payload == "on" {
+			setDebugEnabled(true)
+		} else {
+			setDebugEnabled(false)
+		}
+		sendMessageToJS("debugStatus", payload)
+	case "memoryRead":
+		// Read a memory address and send value back
+		var req struct {
+			Addr int `json:"addr"`
+		}
+		if err := json.Unmarshal([]byte(payload), &req); err == nil {
+			mem := emulator.CPU.GetMemory()
+			if req.Addr >= 0 && req.Addr < len(mem) {
+				msg := struct {
+					Addr  int  `json:"addr"`
+					Value byte `json:"value"`
+				}{Addr: req.Addr, Value: mem[req.Addr]}
+				resp, _ := json.Marshal(msg)
+				sendMessageToJS("memoryReadResult", string(resp))
+			}
+		}
+	case "registersRead":
+		// Send current registers to JS
+		if emulator != nil && emulator.CPU != nil {
+			regJson, _ := json.Marshal(emulator.CPU.GetRegisters())
+			sendMessageToJS("registers", string(regJson))
+		}
+	case "registersWrite":
+		// Update registers from JS
+		if emulator != nil && emulator.CPU != nil {
+			switch c := emulator.CPU.(type) {
+			case *cpu.I8080:
+				var regs cpu.I8080Registers
+				if err := json.Unmarshal([]byte(payload), &regs); err == nil {
+					c.SetRegisters(regs)
+				}
+			case *cpu.Z80:
+				var regs cpu.Z80Registers
+				if err := json.Unmarshal([]byte(payload), &regs); err == nil {
+					c.SetRegisters(regs)
+				}
+			}
+		}
 	case "defineMachine":
 		// Parse config and (re)initialize emulator
 		var config EmulatorConfig
@@ -43,7 +127,17 @@ func receiveMessageFromJS(_ js.Value, args []js.Value) interface{} {
 			sendMessageToJS("machineStatus", "error: "+err.Error())
 			return nil
 		}
-		emulator = &Emulator{Config: config}
+		var cpuImpl CPU
+		switch config.CPU {
+		case "i8080":
+			cpuImpl = cpu.NewI8080(config.RAM)
+		case "z80":
+			cpuImpl = cpu.NewZ80(config.RAM)
+		default:
+			sendMessageToJS("machineStatus", "error: unsupported CPU type")
+			return nil
+		}
+		emulator = &Emulator{Config: config, CPU: cpuImpl}
 		sendMessageToJS("machineStatus", "ok")
 	case "consoleIn":
 		// For now, just echo input back to terminal
